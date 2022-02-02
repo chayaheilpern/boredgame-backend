6a96aa105c2d2de49bd7cdfe6f5885901c9668b1
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-02-02 15:50:13 -0500

    .

--- src/controllers/userControls/authRequired.js
+++ src/controllers/userControls/authRequired.js
@@ -1,27 +1,28 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
+import errorHandler from "../../utilities/error.js";
 
 export const authRequired = (req, res, next) => {
 	const SECRET = process.env.SECRET || "home";
   // const token = req.cookies.jwt;
-  console.log(SECRET);
-  console.log(req.headers.authorization);
-  console.log(req.headers);
-  const token = req.headers.authorization;
-  console.log(token);
-  
+  // console.log(SECRET);
+  // console.log(req.headers.cookie);
+  // console.log(req.headers);
+  const stoken = req.headers.cookie;
+  const token = stoken.split("=")[1];
+  // console.log(token);
+
 	if (token) {
 		jwt.verify(token, SECRET, (error, decodedTkn) => {
 			if (error) {
-				return res
-					.status(401)
-					.json(errorHandler(true, "Auth Error"))
+				return res.json(errorHandler(true, "first"))
 					// .redirect("/login");
-			} else {
+      } else {
+        res.send(decodedTkn)
 				next();
 			}
 		});
 	} else {
-		res.status(401).json(errorHandler(true, "Auth Error"));
+		res.json(errorHandler(true, "second"));
 	}
 };
