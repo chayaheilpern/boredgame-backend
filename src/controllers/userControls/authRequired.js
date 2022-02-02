6876cb97df3355eec336f726ccf7bba9b83884f7
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-02 16:19:15 -0500

    Implement Maya's changes and cleanup formatting.

--- src/controllers/userControls/authRequired.js
+++ src/controllers/userControls/authRequired.js
@@ -4,25 +4,17 @@ import errorHandler from "../../utilities/error.js";
 
 export const authRequired = (req, res, next) => {
 	const SECRET = process.env.SECRET || "home";
-  // const token = req.cookies.jwt;
-  // console.log(SECRET);
-  // console.log(req.headers.cookie);
-  // console.log(req.headers);
-  const stoken = req.headers.cookie;
-  const token = stoken.split("=")[1];
-  // console.log(token);
+	const token = req.headers.cookie.split("=")[1];
 
 	if (token) {
 		jwt.verify(token, SECRET, (error, decodedTkn) => {
 			if (error) {
 				return res.json(errorHandler(true, "first"))
-					// .redirect("/login");
-      } else {
-        res.send(decodedTkn)
+			} else {
 				next();
-			}
+			};
 		});
 	} else {
 		res.json(errorHandler(true, "second"));
-	}
+	};
 };
