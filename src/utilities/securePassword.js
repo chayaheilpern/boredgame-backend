55890eb091b316fc47c2ade0f4e7ebf230e4e185
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-02-02 11:24:12 -0500

    changed bcrypt package to bcryptjs

--- src/utilities/securePassword.js
+++ src/utilities/securePassword.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 
 export const securePassword = async password => {
-	const salt = await bcrypt.genSalt(10); // adding 10 random chars
+	const salt = await bcrypt.genSalt(10);
 	const hashedPassword = await bcrypt.hash(password, salt); // hash pw w/ special/extra 10 chars
 	return hashedPassword;
 };
