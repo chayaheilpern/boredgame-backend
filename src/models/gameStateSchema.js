7b675eb731180a005d3b90f9bf5e1c4164634f7a
Maya Heilpern <maya@maya-heilpern.myfiosgateway.com>
2022-02-03 11:38:06 -0500

    edited gamstate schema

--- src/models/gameStateSchema.js
+++ src/models/gameStateSchema.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
+import territorySchema from "./territorySchema.js";
 
 // Schema of state of a game when saving to DB:
 const gameStateSchema = mongoose.Schema(
     {
-        playerNames: [{playerSchema}],          // Names of players entered on starting a new game.
-        territories: [{territorySchema}],       // 42 Territories on a board. This also represents the territory cards.
+        // playerNames: [{playerSchema}],          // Names of players entered on starting a new game.
+        territories: [territorySchema],       // 42 Territories on a board. This also represents the territory cards.
         turn: { type: String, required: true }  // Name of player who's turn it is currently.
     },
     { timestamps: true }      
 );
 
-const GameState = mongoose.model("gameState", gameStateSchema);
-
-export default GameState;
+export const GameState = mongoose.model("gameState", gameStateSchema);
+export const Territory = mongoose.model("territory", territorySchema);
