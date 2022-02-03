f5aacc0978f11d2896ded6fa13a45a65132d8911
nickdbmiller <nickbarrettmiller@gmail.com>
2022-02-03 10:34:02 -0500

    Add gameStateSchema object to user schema.

--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import gameStateSchema from "./gameStateSchema.js";
 
 const userSchema = mongoose.Schema(
     {
         userName: { type: String, required: true, trim: true },
         email: { type: String, required: true, trim: true },
         password_digest: { type: String, required: true },
-        // games: [{gameSchema}]
+        games: [{gameStateSchema}]
     },
     { timestamps: true }      
 );
